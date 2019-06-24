aa3c3337339002f3fd562db5c321a26d8b49488c
BryanGama <bryangaso15@gmail.com>
2019-06-24 11:37:50 -0500

    fix: Change jQuery to https - add url static files

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('clasificadorSpam.urls')),
     path('admin/', admin.site.urls)
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
