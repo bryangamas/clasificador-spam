d3c49f262b6e8404366c1e8b87e98e1dd7f91733
bgamas <bryangaso15@gmail.com>
2020-11-11 05:20:43 -0500

    fix: test 4

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/prodstatic/')
+STATIC_ROOT = '/var/www/mysite/static/'
 
 TATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    '/var/www/mysite/static/',
 ]
