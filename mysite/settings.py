d03cae02a83b920420bc4ffde570fdba3c4da73b
bgamas <bryangaso15@gmail.com>
2020-11-11 05:29:12 -0500

    fix: test 5

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,10 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/mysite/static/'
 
-TATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/mysite/static/',
-]
+
+django_heroku.settings(locals())
